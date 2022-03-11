99a459c06e0477ac3b77d67b479cd2bf97391bb7
bogdanbojan <bogdanbojan03@gmail.com>
2022-03-11 19:32:11 +0200

    added logger and some temp structure

--- web/handlers.go
+++ web/handlers.go
@@ -1,1 +1,42 @@
-package web
+package main
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func (app *application) howMuch(w http.ResponseWriter, r *http.Request) {
+	handleOtherRequestMethods(w, r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("Creating something.."))
+
+}
+
+func (app *application) howMany(w http.ResponseWriter, r *http.Request) {
+	handleOtherRequestMethods(w, r)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("Creating something.."))
+
+}
+
+func handleOtherRequestMethods(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.Header().Set("Allow", "GET")
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	return
+}
+
+func verifyPayDate(w http.ResponseWriter, r *http.Request) {
+	pd, err := strconv.Atoi(r.URL.Query().Get("pay-date"))
+	if err != nil || pd < 1 {
+		http.NotFound(w, r)
+		return
+	}
+}
+
+func verifyPeriod(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ web/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("salary/how-much", app.howMuch)
+	mux.HandleFunc("salary/how-many", app.howMany)
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
