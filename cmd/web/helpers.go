ec08b28803e8d136d1a3668be061b592cf0cf35f
bogdanbojan <bogdanbojan03@gmail.com>
2022-03-14 19:23:32 +0200

    added tests for the writeJSON function.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -25,6 +25,7 @@ func Date(year int, month time.Month, day int) time.Time {
 	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 }
 
+// TODO: maybe- inject currentDate. It will look weird in whenSalaryYear
 func getAPIDates(payDay int) (currentDate time.Time, salaryDate time.Time) {
 	y, m, d := time.Now().Date()
 	currentDate = Date(y, m, d)
--- cmd/web/response_test.go
+++ cmd/web/response_test.go
@@ -1,1 +1,90 @@
 package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestWriteJSON(t *testing.T) {
+	for _, mc := range writeJSONCases.monthlyCases {
+		rr := httptest.NewRecorder()
+		err := app.writeJSON(rr, http.StatusOK, mc, nil)
+		if err != nil {
+			return
+		}
+		rs := rr.Result()
+		body, err := ioutil.ReadAll(rs.Body)
+		if err != nil {
+			return
+		}
+		bodyCase, _ := json.MarshalIndent(mc, "", "\t")
+		bodyCase = append(bodyCase, '\n')
+
+		assertWriteJSON(t, body, bodyCase)
+		err = rs.Body.Close()
+		if err != nil {
+			return
+		}
+	}
+	for _, yc := range writeJSONCases.yearlyCases {
+		rr := httptest.NewRecorder()
+		err := app.writeJSON(rr, http.StatusOK, yc, nil)
+		if err != nil {
+			return
+		}
+		rs := rr.Result()
+		body, err := ioutil.ReadAll(rs.Body)
+		if err != nil {
+			return
+		}
+		bodyCase, _ := json.MarshalIndent(yc, "", "\t")
+		bodyCase = append(bodyCase, '\n')
+
+		assertWriteJSON(t, body, bodyCase)
+		err = rs.Body.Close()
+		if err != nil {
+			return
+		}
+	}
+
+}
+
+func assertWriteJSON(t testing.TB, got, want []byte) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+var writeJSONCases = struct {
+	monthlyCases []Monthly
+	yearlyCases  []Yearly
+	//wantMonthlyCases []string
+	//wantYearlyCases  []string
+}{
+	monthlyCases: []Monthly{
+		{
+			NrOfDays: 16,
+			Date:     "Tuesday, 15-Mar-22",
+		},
+	},
+	yearlyCases: []Yearly{
+		{
+			Dates: []string{
+				"Tuesday, 15-Mar-22",
+				"Friday, 15-Apr-22",
+				"Friday, 20-May-22",
+				"Wednesday, 15-Jun-22",
+				"Friday, 15-Jul-22",
+				"Monday, 15-Aug-22",
+				"Thursday, 15-Sep-22",
+				"Friday, 21-Oct-22",
+				"Tuesday, 15-Nov-22",
+				"Thursday, 15-Dec-22",
+			},
+		},
+	},
+}
